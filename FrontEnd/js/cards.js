9592e30c5679113b784b37ee03714fa5182dec2f
jogebro <joelabril2110@gmail.com>
2023-10-05 13:54:40 -0500

    feat: :sparkles: creacion de tienda en front

--- FrontEnd/js/cards.js
+++ FrontEnd/js/cards.js
@@ -4,19 +4,36 @@ document.addEventListener('DOMContentLoaded', ()=>{
     llenarCards();
 })
 
-
-async function llenarCards(){
+async function llenarCards() {
     const datos = await getAllGames();
-    console.log(datos);
     const cards = document.querySelector('.container-cards');
-    datos.forEach(dato=>{
-        const { Nombre, Imagen, Precio} = dato;
-        cards.innerHTML+=`
+    
+    
+    datos.forEach(dato => {
+        const { Nombre, Imagen, Precio } = dato;
+        const precioFormateado = Precio.toLocaleString();
+        cards.innerHTML += `
+        <a href="pagina_destino.html">
             <div class="card">
-                <img src="./images/${Imagen}" alt="Imagen de la tarjeta">
+            <img src="../images/${Imagen}" alt="Imagen de la tarjeta">
+            <div class="card-detalles">
                 <h2>${Nombre}</h2>
-                <p>${Precio}</p>
+                <p>$${precioFormateado} COP</p>
             </div>
+            <button id="buyButton">Comprar</button>
+            </div>
+        </a>
         `
-    })
+    });
+}
+
+document.addEventListener('click', function (e) {
+    if (e.target && e.target.id == 'buyButton') {
+        e.preventDefault();
+        alertCompra();
+    }
+});
+
+function alertCompra() {
+    alert('Esta función no está implementada');
 }
